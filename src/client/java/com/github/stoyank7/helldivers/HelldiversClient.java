49a33fbcd11275c7c58206f69edb6eed877c50da
stoyan <stoyank127@gmail.com>
2025-05-01 15:27:19 +0200

    initialize project from fabric template

---
+++ src/client/java/com/github/stoyank7/helldivers/HelldiversClient.java
@@ -0,0 +1,10 @@
+package com.github.stoyank7.helldivers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelldiversClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
